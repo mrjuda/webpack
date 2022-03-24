f4e2dc6e745381cf73cef0b5ad7df90e8efd85ba
judah <87juda@gmail.com>
2022-03-23 21:55:27 -0300

    Set up local server

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,20 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: {
     index: './src/index.js',
     print: './src/print.js',
   },
   plugins: [
     new HtmlWebpackPlugin({
+      title: 'Development',
       template: './src/index.html'
     }),
   ],
+  devServer: {
+    static: './dist',
+  },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
