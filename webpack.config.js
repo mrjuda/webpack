d86589cfb2ade203b42c906a5dcdf63a11a6da8e
judah <87juda@gmail.com>
2022-03-23 21:57:08 -0300

    Set up local server

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Development',
-      template: './src/index.html'
+      template: './src/index.html',
     }),
   ],
   devServer: {
